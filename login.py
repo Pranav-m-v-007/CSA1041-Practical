83a5cf45587600f82bbefc52235e051bf30b2383
Pranav-m-v-007 <mvpranav11@gmail.com>
2026-03-26 13:35:35 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,11 @@
 ur=Pranav
 pd=1234
+ur1=str(input("Enter User Name")
+pd1=int(input("Enter Pin")
 if (ur==ur1):
-  print("login Successfull")
+  if (pd==pd1):
+    print("login Successfull")
+  else:
+    print("Password is incorrect")       
 else:
-  print("Username/Password is incorrect")
+  print("Username incorrect")
